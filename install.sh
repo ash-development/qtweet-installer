2ea28a63ab6b96cfe6bf1fad9872e98c4b366ccd
ash <killua@schnoor.email>
2021-03-22 11:39:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,31 +2,31 @@
 
 set -e
 
-#################################################################################
-#                                                                               #
-# Project 'qtweet-installer'                                                    #
-#                                                                               #
-# Copyright (C) 2021, Killua Schnoor, <killua@schnoor.email>                    #
-#                                                                               #
-#   This program is free software: you can redistribute it and/or modify        #
-#   it under the terms of the GNU General Public License as published by        #
-#   the Free Software Foundation, either version 3 of the License, or           #
-#   (at your option) any later version.                                         #
-#                                                                               #
-#   This program is distributed in the hope that it will be useful,             #
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of              #
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
-#   GNU General Public License for more details.                                #
-#                                                                               #
-#   You should have received a copy of the GNU General Public License           #
-#   along with this program.  If not, see <https://www.gnu.org/licenses/>.      #
-#                                                                               #
-# https://github.com/thekilluadev/qtweet-installer/new/main/blob/master/LICENSE #
-#                                                                               #
-# This script is not associated with the official QTweet Project.               #
-# https://github.com/thekilluadev/qtweet-installer/new/main                     #
-#                                                                               #
-#################################################################################
+####################################################################################
+#                                                                                  #
+# Project 'qtweet-installer'                                                       #
+#                                                                                  #
+# Copyright (C) 2021, Ash Schnoor, <ash@schnoor.email>                             #
+#                                                                                  #
+#   This program is free software: you can redistribute it and/or modify           #
+#   it under the terms of the GNU General Public License as published by           #
+#   the Free Software Foundation, either version 3 of the License, or              #
+#   (at your option) any later version.                                            #
+#                                                                                  #
+#   This program is distributed in the hope that it will be useful,                #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
+#   GNU General Public License for more details.                                   #
+#                                                                                  #
+#   You should have received a copy of the GNU General Public License              #
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.         #
+#                                                                                  #
+# https://github.com/ash-development/qtweet-installer/new/main/blob/master/LICENSE #
+#                                                                                  #
+# This script is not associated with the official QTweet Project.                  #
+# https://github.com/thekilluadev/qtweet-installer/new/main                        #
+#                                                                                  #
+####################################################################################
 
 SCRIPT_VERSION="v0.1.0"
 
